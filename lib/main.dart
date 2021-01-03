e18428ebbf395a3eabfc91ca80144a613c080717
rohangoli <rohanr27@gmail.com>
2021-01-03 23:30:57 +0530

    Add new route for Saved Suggestions

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -82,6 +85,36 @@ class RandomWordsState extends State<RandomWords> {
           }
         });
       },
+    );
+  }
+
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final Iterable<ListTile> tiles = _saved.map(
+                (WordPair pair){
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    )
+                  );
+                }
+            );
+            final List<Widget> divided = ListTile.divideTiles(
+                tiles: tiles,
+                context: context,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          }
+      )
     );
   }
 }
